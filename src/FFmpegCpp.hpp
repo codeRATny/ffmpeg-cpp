9b29282a7361e0117a0ed7f1e0accbf6dc347421
Баяндин Сергей Андреевич <sergey.bayandin@eltex.loc>
2023-07-25 13:16:29 +0700

    Created header file for include all libraries

---
+++ src/FFmpegCpp.hpp
@@ -0,0 +1,12 @@
+#ifndef _FFMPEG_CPP_HPP_
+#define _FFMPEG_CPP_HPP_
+
+#include "FFmpegCodecCtx.hpp"
+#include "FFmpegFormatCtx.hpp"
+#include "FFmpegSwsCtx.hpp"
+#include "FFmpegFrame.hpp"
+#include "FFmpegIOCtx.hpp"
+#include "FFmpegPacket.hpp"
+#include "Utils.hpp"
+
+#endif
